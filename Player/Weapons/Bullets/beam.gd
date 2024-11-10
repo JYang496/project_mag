6d42dab1e5f9d887b5da1e4f7fdfb0f12f785e03
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-11-09 23:00:54 -0500

    1109

--- Player/Weapons/Bullets/beam.gd
+++ Player/Weapons/Bullets/beam.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+@onready var raycast = $RayCast2D
+@onready var line = $RayCast2D/Line2D
+
+var target_position : Vector2 = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	line.show()
+	line.add_point(Vector2.ZERO)
+	line.add_point(raycast.target_position)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_expire_timer_timeout() -> void:
+	pass # Replace with function body.
