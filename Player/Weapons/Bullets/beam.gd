a89545991cce700444cbce2c9dec0e9a536f8238
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-11-11 21:18:50 -0500

    1111

--- Player/Weapons/Bullets/beam.gd
+++ Player/Weapons/Bullets/beam.gd
@@ -5,21 +5,25 @@ extends Node2D
 
 var target_position : Vector2 = Vector2.ZERO
 
+var attack : Attack
+var damage = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	raycast.target_position = target_position
 	line.show()
-	print(raycast.target_position)
-	pass
 
 func _physics_process(delta: float) -> void:
 	if raycast.is_colliding():
 		var collision_point = raycast.get_collision_point()
 		var collider = raycast.get_collider()
+
 		line.points = [Vector2.ZERO, to_local(collision_point)]
-		print(line.points)
-	pass
+		if collider is HurtBox:
+			var target = collider.get_owner()
+			attack = Attack.new()
+			attack.damage = damage
+			target.damaged(attack)
 
 func _on_expire_timer_timeout() -> void:
 	self.call_deferred("queue_free")
--- Player/Weapons/laser.gd
+++ Player/Weapons/laser.gd
@@ -4,13 +4,59 @@ extends Ranger
 @onready var beam = preload("res://Player/Weapons/Bullets/beam.tscn")
 
 @onready var cooldown_timer = $LaserCooldownTimer
+
+# Weapon
+var ITEM_NAME = "Mini Gun"
+var level : int
+var damage : int
+var speed : int
+var hp : int
+var reload : float
+
+
+var weapon_data = {
+	"1": {
+		"level": "1",
+		"damage": "1",
+		"reload": "2",
+		"cost": "1",
+		"features": [],
+	},
+	"2": {
+		"level": "2",
+		"damage": "2",
+		"reload": "2",
+		"cost": "1",
+		"features": [],
+	},
+	"3": {
+		"level": "3",
+		"damage": "3",
+		"reload": "2",
+		"cost": "1",
+		"features": [],
+	},
+	"4": {
+		"level": "4",
+		"damage": "4",
+		"reload": "1",
+		"cost": "1",
+		"features": [],
+	},
+	"5": {
+		"level": "5",
+		"damage": "5",
+		"reload": "2",
+		"cost": "1",
+		"features": [],
+	}
+}
 func _ready():
 	pass
 
 func _on_shoot():
 	var beam_ins = beam.instantiate()
 	beam_ins.global_position = self.global_position
-	print(get_global_mouse_position())
 	beam_ins.target_position = get_global_mouse_position() - self.global_position
 	self.get_tree().root.call_deferred("add_child",beam_ins)
 	justAttacked = true
