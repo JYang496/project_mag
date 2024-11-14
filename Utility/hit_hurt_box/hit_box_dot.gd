c9c4f5a6dc85df4334a1ecac7f7ea1003c933856
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-11-13 21:31:37 -0500

    1113

--- Utility/hit_hurt_box/hit_box_dot.gd
+++ Utility/hit_hurt_box/hit_box_dot.gd
@@ -7,12 +7,10 @@ class_name HitBoxDot
 var attack : Attack
 var cooldown = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	for area in get_overlapping_areas():
 		if area is HurtBox and not cooldown:
