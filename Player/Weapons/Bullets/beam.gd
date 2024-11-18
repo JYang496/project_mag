0680f2a6df0f78a67512a8b4ffcc13b840ea3685
Jimmy <128646419+JYang496@users.noreply.github.com>
2024-11-18 16:28:56 -0500

    1118

--- Player/Weapons/Bullets/beam.gd
+++ Player/Weapons/Bullets/beam.gd
@@ -9,12 +9,18 @@ var target_position : Vector2 = Vector2.ZERO
 var attack : Attack
 var damage = 1
 
+var frame_counter = 0
+var frames_until_show = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	raycast.target_position = 42 * target_position
-	line.show()
 
 func _physics_process(delta: float) -> void:
+	frame_counter += 1
+	if frame_counter > frames_until_show:
+		line.show()
 	if raycast.is_colliding():
 		var collision_point = raycast.get_collision_point()
 		var collider = raycast.get_collider()
@@ -25,6 +31,8 @@ func _physics_process(delta: float) -> void:
 			attack = Attack.new()
 			attack.damage = damage
 			target.damaged(attack)
+	else:
+		line.points = [Vector2.ZERO, to_local(raycast.target_position)]
 
 func _on_expire_timer_timeout() -> void:
 	self.call_deferred("queue_free")
