f66b7fa4be14eaaa3d2ee78a3dfa71b2f231f5d8
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-11-21 20:43:22 -0500

    1121

--- Player/Weapons/machine_gun.gd
+++ Player/Weapons/machine_gun.gd
@@ -13,12 +13,13 @@ var damage : int
 var speed : int
 var hp : int
 var reload : float
+var attack_speed : float = 1.0
 
 
 var weapon_data = {
 	"1": {
 		"level": "1",
-		"damage": "10",
+		"damage": "5",
 		"speed": "600",
 		"hp": "1",
 		"reload": "1",
@@ -27,37 +28,37 @@ var weapon_data = {
 	},
 	"2": {
 		"level": "2",
-		"damage": "15",
+		"damage": "5",
 		"speed": "600",
 		"hp": "1",
-		"reload": "1",
+		"reload": "0.8",
 		"cost": "1",
 		"features": [],
 	},
 	"3": {
 		"level": "3",
-		"damage": "20",
+		"damage": "7",
 		"speed": "600",
 		"hp": "1",
-		"reload": "1",
+		"reload": "0.7",
 		"cost": "1",
 		"features": [],
 	},
 	"4": {
 		"level": "4",
-		"damage": "20",
+		"damage": "9",
 		"speed": "800",
 		"hp": "1",
-		"reload": "0.75",
+		"reload": "0.65",
 		"cost": "1",
 		"features": [],
 	},
 	"5": {
 		"level": "5",
-		"damage": "25",
+		"damage": "11",
 		"speed": "800",
 		"hp": "2",
-		"reload": "0.75",
+		"reload": "0.6",
 		"cost": "1",
 		"features": ["piercing"],
 	}
@@ -83,6 +84,7 @@ func set_level(lv):
 	
 func _on_shoot():
 	justAttacked = true
+	gun_cooldownTimer.wait_time = reload / attack_speed
 	gun_cooldownTimer.start()
 	var spawn_bullet = bullet.instantiate()
 	var bullet_direction = global_position.direction_to(get_random_target()).normalized()
@@ -93,8 +95,15 @@ func _on_shoot():
 	apply_linear(spawn_bullet, bullet_direction, speed)
 	apply_affects(spawn_bullet)
 	get_tree().root.call_deferred("add_child",spawn_bullet)
+	adjust_attack_speed(1.2)
 
-
+func adjust_attack_speed(rate : float) -> void:
+	attack_speed = clampf(attack_speed * rate, 1.0, 10.0)
 
 func _on_machine_gun_timer_timeout() -> void:
 	justAttacked = false
+
+
+func _on_as_timer_timeout() -> void:
+	if not justAttacked:
+		adjust_attack_speed(0.5)
