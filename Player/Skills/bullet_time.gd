6ba3589a6fb3891933194573149bd91ca489bdbf
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-12-23 21:50:47 -0500

    bullet time

--- Player/Skills/bullet_time.gd
+++ Player/Skills/bullet_time.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var time_scale = 0.5
+var saved_scale
+var saved_speed
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var timer: Timer = $Timer
 
@@ -13,5 +15,13 @@ func _ready() -> void:
 	
 func _on_active_skill() -> void:
 	timer.start()
+	saved_scale = Engine.time_scale
 	Engine.time_scale = time_scale
-	
+	saved_speed = PlayerData.player_speed * 3
+	PlayerData.player_bonus_speed += saved_speed
+
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = saved_scale
+	PlayerData.player_bonus_speed -= saved_speed
