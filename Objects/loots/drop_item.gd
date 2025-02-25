2d02f1b71ace74b5b9575db0392a81fe21fa6142
Jimmy <128646419+JYang496@users.noreply.github.com>
2025-02-25 16:17:15 -0500

    loot

--- Objects/loots/drop_item.gd
+++ Objects/loots/drop_item.gd
@@ -3,8 +3,10 @@ extends Node2D
 var item_id : String = "1"
 var level := 1
 var item : Node2D
+var player_near : bool = false
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var detect_area: Area2D = $DetectArea
+@onready var interact_hint: Label = $InteractHint
 
 
 func _ready() -> void:
@@ -12,20 +14,24 @@ func _ready() -> void:
 		item = load(WeaponData.weapon_list.data[item_id]["res"]).instantiate()
 		sprite.texture = load(WeaponData.weapon_list.data[item_id]["img"])
 		item.level = level
+		play_animation()
 	
 func play_animation() -> void:
 	var start_position = position
-	var rand_pos = get_random_position_in_circle()
 	var dest_tween = create_tween()
 	dest_tween.tween_property(self,"rotation_degrees", 1800, 1).set_ease(Tween.EASE_IN_OUT)
 	dest_tween.connect("finished", _on_dest_tween_finished)
 
-func get_random_position_in_circle(radius: float = 50.0) -> Vector2:
-	var angle = randf_range(0, TAU)  # TAU is 2*PI in Godot
-	var distance = randf() * radius  # Random distance between 0 and radius
-	var x = cos(angle) * distance
-	var y = sin(angle) * distance
-	return Vector2(x, y)
-
 func _on_dest_tween_finished():
-	print("finish")
+	detect_area.set_collision_mask_value(1,true)
+
+func _on_detect_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		interact_hint.visible = true
+		player_near = true
+
+
+func _on_detect_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		interact_hint.visible = false
+		player_near = false
--- Objects/loots/loot_box.gd
+++ Objects/loots/loot_box.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var coin_preload = preload("res://Objects/loots/coin.tscn")
+@onready var item_preload = preload("res://Objects/loots/drop_item.tscn")
 @onready var drop_preload = preload("res://Objects/loots/drop.tscn")
 @onready var disapear_timer: Timer = $DisapearTimer
 
@@ -23,6 +24,10 @@ func animation() -> void:
 	tween.connect("finished",_on_tween_finished)
 
 func drops() -> void:
+	var item_drop = drop_preload.instantiate()
+	item_drop.drop = item_preload
+	item_drop.global_position = self.global_position
+	self.call_deferred("add_sibling",item_drop)
 	for i in range(number_of_coins):
 		var bonus := 0
 		if remainder >= 0 and remainder <= 10:
