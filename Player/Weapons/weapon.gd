20fd9989802708ecaca66373eca0c00d9aa8b2a2
Jimmy <128646419+JYang496@users.noreply.github.com>
2025-03-24 16:28:41 -0400

    weapon scene

--- Player/Weapons/weapon.gd
+++ Player/Weapons/weapon.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Weapon
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var modules: Node2D = $Modules
 
 # Common variables for weapons
 var level : int
