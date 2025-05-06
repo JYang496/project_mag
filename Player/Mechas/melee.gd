95e209a91723071806b1a0cfb743bf00a0d6a00b
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2025-05-05 22:23:41 -0400

    player spawner

---
+++ Player/Mechas/melee.gd
@@ -0,0 +1,6 @@
+extends Player
+
+
+func custom_ready() -> void:
+	create_weapon("7")
+	print("overwrite from melee")
---
+++ Player/Mechas/ranger.gd
@@ -0,0 +1,5 @@
+extends Player
+
+func custom_ready() -> void:
+	create_weapon("1")
+	print("overwrite from ranger")
